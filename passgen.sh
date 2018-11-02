e51783a5198c9c9675416cc4687761a86745a1c5
Zaur Molotnikov <qutorial@gmail.com>
2018-11-02 09:59:19 +0100

    Fixed i and trash in the main file

--- passgen.sh
+++ passgen.sh
@@ -1,11 +1,1 @@
-alias passgen="apg -a0 -m40 | tr 'z' 'D' | tr 'Z' '4' | tr 'y' 'H' | tr 'Y' 'R' | tr 'I' 'B' | tr 'l' '8' | tr 'I' 'X' | tr '1' 'M' | tr '0' 'U' | tr 'O' 'p' | tr 'J' 'q' "
-
-$ passgen                                                                                                                
-casaspokmocdegCufMarkakKequarnHonFicfid:                                                                                                   
-kicdeemockVipvchenHipdaji8pecVuBckCeHWHn                                                                                                   
-seppsujiahHtAnRHnnobwe8kAsukir8EadwodsBt                                                                                                   
-meepnuPepjoshborWaunEcpekBdHibNuntUg4or~                                                                                                   
-Thintu8fafhHWadHohabHoukpavroofDeawthEth                                                                                                   
-Had2two8CocaktenqefkuAvHi88Bshbeshice8ad 
-
-
+alias passgen="apg -a0 -m40 | tr 'z' 'D' | tr 'Z' '4' | tr 'y' 'H' | tr 'Y' 'R' | tr 'I' 'B' | tr 'l' '8' | tr 'I' 'X' | tr '1' 'M' | tr '0' 'U' | tr 'O' 'p' | tr 'J' 'q' | tr 'i' 'F'"
